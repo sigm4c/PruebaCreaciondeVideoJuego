f83a927755d09c3d369bce5e842cc1ba43d3d1df
sigm4c <84212258+sigm4c@users.noreply.github.com>
2023-01-15 10:45:49 -0400

    se agrega el proyecto

---
+++ control.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    private bool isFacingRight = true;
+    public float velocidad;
+    private new Rigidbody2D  rigidbody;
+    public float FuerzaSalto;
+
+
+
+    [SerializeField] private float VelocidadDash;
+    [SerializeField] private float TiempoDash;
+
+    private float GravedadInicial;
+    private bool puedeHacerDash=true;
+    private bool SePuedeMover=true;
+
+    private void Start()
+    {
+        rigidbody= GetComponent<Rigidbody2D>();
+
+        GravedadInicial = rigidbody.gravityScale;
+
+    }
+    void Update()
+    {
+        if (SePuedeMover) { 
+        ProcesarMovimiento();
+        
+        }
+        ProcesarSalto();
+
+
+
+        if(Input.GetKeyDown(KeyCode.Z) && puedeHacerDash)
+        {
+            
+
+            StartCoroutine(Dash());
+        }
+    }
+
+    void ProcesarMovimiento()
+    {
+        float inputMovimiento = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(inputMovimiento * velocidad, rigidbody.velocity.y);
+        GestionarOrientacion(inputMovimiento);
+    }
+    void ProcesarSalto()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector2.up*FuerzaSalto,ForceMode2D.Impulse);
+        }
+    }
+
+    void GestionarOrientacion(float inputMovimiento)
+    {
+        if(isFacingRight && inputMovimiento < 0f || !isFacingRight && inputMovimiento > 0) {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        SePuedeMover = false;
+        puedeHacerDash = false;
+        rigidbody.gravityScale = 0;
+        rigidbody.velocity = new Vector2(VelocidadDash*transform.localScale.x,0);
+
+        yield return new WaitForSeconds(TiempoDash);
+        SePuedeMover = true;
+        puedeHacerDash=true;
+        rigidbody.gravityScale = GravedadInicial;
+
+    }
+    
+
+}
---
+++ teleport.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    new public Collider2D Collider;
+    public LayerMask layer;
+    public Transform destinationTransform;
+    public float delayTime = 0.3f;
+
+    WaitForSeconds delay;
+
+    IEnumerator Activate(GameObject teleportee)
+    {
+        if (destinationTransform)
+        {
+            yield return delay;
+            teleportee.transform.position = destinationTransform.position;
+            teleportee.transform.rotation = destinationTransform.rotation;
+        }
+    }
+    void Start()
+    {
+      Collider= GetComponent<Collider2D>();
+        Collider.isTrigger= true;
+    }
+    void Awake()
+    {
+         delay = new WaitForSeconds(delayTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isTeleportable(other)) StartCoroutine(Activate(other.gameObject));
+        
+    }
+    bool isTeleportable(Collider2D other) { return 0 != (layer.value & 1 << other.gameObject.layer); }
+
+
+}
